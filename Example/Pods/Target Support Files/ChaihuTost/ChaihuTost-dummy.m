789a6d5f8615754a86e84349a6cc455832bd0e59
shanli <shanli@2dfire.com>
2017-08-21 19:25:51 +0800

    add Library

---
+++ Example/Pods/Target Support Files/ChaihuTost/ChaihuTost-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ChaihuTost : NSObject
+@end
+@implementation PodsDummy_ChaihuTost
+@end
---
+++ Example/Pods/Target Support Files/ChaihuTost/ChaihuTost-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double ChaihuTostVersionNumber;
+FOUNDATION_EXPORT const unsigned char ChaihuTostVersionString[];
+
